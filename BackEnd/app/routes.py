8f7ce53ea7a235311bb02b68032dd6d69b9eab15
Rohan <rohan.kamath.24@gmail.com>
2025-07-05 08:08:45 +0530

    Added an endpoint for chat

--- BackEnd/app/routes.py
+++ BackEnd/app/routes.py
@@ -61,4 +61,33 @@ async def index_docs(request: Request, index_request_data: IndexRequest):
     except Exception as e:
         print(f"[{__name__}] ERROR during indexing: {e}")
         raise HTTPException(status_code=500, detail=f"An unexpected error occurred during indexing: {str(e)}")
-        
+    
+@router.post("/chat", response_model=ChatResponse)
+async def chat_with_bot(request: Request, chat_request_data: ChatRequest):
+    """
+    This is the endpoint for chatting with the bot
+    Expects a single user query
+    Returns an answer(str) and list of sources
+    """
+    retriever_instance = request.app.state.retriever_instance
+    if retriever_instance is None:
+        raise HTTPException(status_code=503, detail="Retriever has not been initialised")
+    
+    user_query = chat_request_data.query
+    print(f"[{__name__}] Received query: {user_query}")
+
+    try:
+        rewritten_query = query_translation(user_query, GOOGLE_API_KEY, DEFAULT_LLM_MODEL)
+        generator = Generation(
+            query = rewritten_query,
+            api_key = GOOGLE_API_KEY,
+            retriever = retriever_instance,
+            model = DEFAULT_LLM_MODEL
+        )
+        answer, sources = generator.generate()
+        print(f"[{__name__}] Generated answer: {answer}")
+        print(f"[{__name__}] Sources: {list(sources)}")
+        return ChatResponse(answer = answer, sources = list(sources))
+    except Exception as e:
+        print(f"[{__name__}] An error occurred: {e}")
+        raise HTTPException(status_code=500, detail="An error occurred")
